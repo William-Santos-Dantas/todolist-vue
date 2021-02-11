841ab34037609c9f98d3aff48ae662ff484d4648
William_Santos_Dantas <NidrahDev@protonmail.com>
2021-02-11 08:48:48 -0300

    fix state

--- src/store/modules/list/mutations.js
+++ src/store/modules/list/mutations.js
@@ -6,9 +6,9 @@ export default {
         state.items = state.items.filter((item) => item.id != id)
     },
     update_item(state, item) {
-        let index = state.items.findIndex(it => it.id == item.id)
+        const index = state.items.findIndex(it => it.id == item.id)
         state.items[index] = item
-        let items = state.items
+        const items = [...state.items]
         state.items = {}
         state.items = items
     }
